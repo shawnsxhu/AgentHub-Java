8fea75238421671c1c8b7383b98fedf2d608666c
Shawn Hu <s1hu@ucsd.edu>
2026-06-04 13:57:50 -0700

    Initialize Spring Boot application

---
+++ agenthub/src/main/java/com/omni/agenthub/AgenthubApplication.java
@@ -0,0 +1,13 @@
+package com.omni.agenthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenthubApplication.class, args);
+	}
+
+}
---
+++ agenthub/src/test/java/com/omni/agenthub/AgenthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omni.agenthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
